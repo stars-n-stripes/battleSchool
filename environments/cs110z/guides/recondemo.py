0159b7e7f4a402c1b74297ba3b4f32967ba80eec
James Lynch <jctlynch@gmail.com>
2020-02-21 07:05:58 -0700

    Changed some names in main.tf to reflect the two-VM nature of the cyber demonstration.

---
+++ environments/cs110z/guides/recondemo.py
@@ -0,0 +1,14 @@
+from prompt_toolkit import prompt
+
+"""
+Reconnaissance Demo
+Author: 2d Lt James Lynch
+Purpose: Provide an interactive REPL loop that allows a student to work through the recon phase of the 
+    Cyber-Attack Methodology 
+
+"""
+
+
+while 1:
+    user_input = prompt('>')
+    print(user_input)
