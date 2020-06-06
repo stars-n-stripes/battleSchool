9ba04ba418e29f8a5b90ce17302dd6079258c570
Chuck Lynch <james@jamesclynch.com>
2020-06-05 23:41:09 -0600

    Added prototype script for pulling and deploying BSSM

---
+++ challenges/test/addSM.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Script to add and build a webservice that is accessible by the student.
+# This webservice allows the student to revert VMs in the scenario and
+# - maybe one day -
+# submit flags.
+
+# Author: starsnstripes
+################################################
+# *  *  *  *  *  * 000000000000000000000000000 #
+#  *  *  *  *  *   111111111111111111111111111 #
+# *  *  *  *  *  * 000000000000000000000000000 #
+#  *  *  *  *  *   111111111111111111111111111 #
+# *  *  *  *  *  * 000000000000000000000000000 #
+#  *  *  *  *  *   111111111111111111111111111 #
+# *  *  *  *  *  * 000000000000000000000000000 #
+#  *  *  *  *  *   111111111111111111111111111 #
+# *  *  *  *  *  * 000000000000000000000000000 #
+# 11111111111111111111111111111111111111111111 #
+# 00000000000000000000000000000000000000000000 #
+# 11111111111111111111111111111111111111111111 #
+# 00000000000000000000000000000000000000000sns #
+################################################
+
+echo "Changing to dev user. . ."
+su dev
+cd /scenario
+
+# Download the codebase
+echo "Downloading BSSM . . ."
+git clone https://github.com/stars-n-stripes/battleSchoolSM
+cd battleSchoolSM
+
+# Run the server 
+# TODO: Detect Vbox IP address during runtime
+echo "Running Scenario Manager Web Interface . . ."
+echo "This will attempt to run via sudo:"
+sudo python manage.py runserver 192.168.33.1 80 &
