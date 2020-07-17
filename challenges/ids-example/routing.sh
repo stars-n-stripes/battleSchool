b506ad9f69c996c694f048a7974b95e15d86fcf0
Chuck Lynch <james@jamesclynch.com>
2020-07-17 23:34:27 +0000

    Added default routing for client machines and sudo for iptables provisioning

--- challenges/ids-example/routing.sh
+++ challenges/ids-example/routing.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+er #!/bin/sh
 
 # Reference: https://www.systutorials.com/setting-up-gateway-using-iptables-and-route-on-linux/
 
@@ -6,10 +6,11 @@
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 
 # Establish forwarding rules via iptables (kali -> victim)
-iptables -t nat -A POSTROUTING ! -d 192.168.31.0/24 -o eth2 -j SNAT --to-source 192.168.32.1
+sudo iptables -t nat -A POSTROUTING ! -d 192.168.31.0/24 -o eth2 -j SNAT --to-source 192.168.32.1
 
 # Establish forwarding rules via iptables (victim -> kali)
-iptables -t nat -A POSTROUTING ! -d 192.168.32.0/24 -o eth1 -j SNAT --to-source 192.168.31.1
+sudo iptables -t nat -A POSTROUTING ! -d 192.168.32.0/24 -o eth1 -j SNAT --to-source 192.168.31.1
 
 # Save forwarding rules in case on unexpected reboot
-iptables-save > /etc/iptables.rules
+# Can't use this right now as su -c (for sudo redirects) requests password auth
+# sudo iptables-save > /etc/iptables.rules
