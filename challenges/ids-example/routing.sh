17965524200d02b501c4093dd3b079beecbc3b0a
Chuck Lynch <james@jamesclynch.com>
2020-07-18 00:07:48 +0000

    Removed sudo after I noticed error on line 1 of routing.sh

--- challenges/ids-example/routing.sh
+++ challenges/ids-example/routing.sh
@@ -1,15 +1,15 @@
-er #!/bin/sh
+#!/bin/sh
 
 # Reference: https://www.systutorials.com/setting-up-gateway-using-iptables-and-route-on-linux/
 
 # Enable IP forwarding
-sudo echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 
 # Establish forwarding rules via iptables (kali -> victim)
-sudo iptables -t nat -A POSTROUTING ! -d 192.168.31.0/24 -o eth2 -j SNAT --to-source 192.168.32.1
+iptables -t nat -A POSTROUTING ! -d 192.168.31.0/24 -o eth2 -j SNAT --to-source 192.168.32.1
 
 # Establish forwarding rules via iptables (victim -> kali)
-sudo iptables -t nat -A POSTROUTING ! -d 192.168.32.0/24 -o eth1 -j SNAT --to-source 192.168.31.1
+iptables -t nat -A POSTROUTING ! -d 192.168.32.0/24 -o eth1 -j SNAT --to-source 192.168.31.1
 
 # Save forwarding rules in case on unexpected reboot
 # Can't use this right now as su -c (for sudo redirects) requests password auth
